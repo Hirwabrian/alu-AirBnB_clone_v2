6209f2e6d76e9f39964eb9fff80b70c299b11d88
Hirwabrian <H.brian@alustudent.com>
2024-09-27 22:56:03 +0200

    done with the project

--- models/place.py
+++ models/place.py
@@ -31,7 +31,7 @@ class Place(BaseModel,Base):
     
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review", backref="place", cascade="all, delete")
-        amenities = relationship("Amenity", secondary=place_amenity, backref="place_amenities", viewonly=False)
+        amenities = relationship("Amenity", secondary=place_amenity, back_populates="place_amenities", viewonly=False)
 
     else:
         @property
