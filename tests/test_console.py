e5eba17b83f2f9386e8919fff96e187d33ba468b
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-09-28 22:25:51 +0200

    dummy test_console.py

---
+++ tests/test_console.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""tests/test_console.py"""
+
+import unittest
+
+class TestConsole(unittest.TestCase):
+    def test_dummy(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
