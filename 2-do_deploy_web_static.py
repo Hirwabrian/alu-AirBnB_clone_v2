74983f86b0979717c36091ff1990b27f7418811e
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-10-05 21:03:16 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,28 +4,38 @@ Fabric script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
 
-from fabric.api import *
-from fabric.operations import run, put, sudo
+from fabric import Connection, task
+from invoke.exceptions import UnexpectedExit
 from os.path import exists
-env.hosts = ['54.227.34.151', '100.27.221.120']
+
+# Define the list of hosts
+env_hosts = ['ubuntu@54.227.34.151', 'ubuntu@100.27.221.120']
 
 
-def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+@task
+def do_deploy(c, archive_path):
+    """Distributes an archive to the web servers"""
+    if not exists(archive_path):
         return False
+    
     try:
         file_name = archive_path.split("/")[-1]
         file_no_extension = file_name.split(".")[0]
         path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, file_no_extension))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, file_no_extension))
-        run('rm /tmp/{}'.format(file_name))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_no_extension))
-        run('rm -rf {}{}/web_static'.format(path, file_no_extension))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, file_no_extension))
+        
+        # Loop over each host to perform the deployment
+        for host in env_hosts:
+            conn = Connection(host)  # Create a connection for each host
+            conn.put(archive_path, '/tmp/')  # Upload the archive
+            conn.run(f'mkdir -p {path}{file_no_extension}/')  # Create directory
+            conn.run(f'tar -xzf /tmp/{file_name} -C {path}{file_no_extension}/')  # Extract the archive
+            conn.run(f'rm /tmp/{file_name}')  # Remove the archive from /tmp
+            conn.run(f'mv {path}{file_no_extension}/web_static/* {path}{file_no_extension}/')  # Move files
+            conn.run(f'rm -rf {path}{file_no_extension}/web_static')  # Clean up
+            conn.run('rm -rf /data/web_static/current')  # Remove old symlink
+            conn.run(f'ln -s {path}{file_no_extension}/ /data/web_static/current')  # Create new symlink
+            
         return True
-    except:
+    except UnexpectedExit as e:
+        print(f"Error: {e}")
         return False
