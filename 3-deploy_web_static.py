26c927f4c9ebc3601f4ea5356e8f18634bf52575
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-10-05 13:18:09 +0200

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,53 +1,30 @@
 #!/usr/bin/python3
-"""a Fabric script that creates and distributes an archive to your web servers"""
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
 
-from fabric.api import local, env, put, run
-import os
-import datetime
-env.hosts = ['54.196.153.63', '54.227.59.25']
-
-def do_pack():
-    """"
-    generates a tgz archive
-    return:  the archive path if the archive has been correctly generated.
-    """
-    try:
-        time = datetime.now().strftime("%Y%m%d%H%M%S")
-        if not os.path.exists("versions"):
-            os.makedirs("versions")
-        path = f"versions/web_static_{time}.tgz"
-        local(f"tar -cvzf {path} web_static")
-        return path
-    except:
-        return None
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['142.44.167.228', '144.217.246.195']
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to web servers
-    returns: True if all operations have been done correctly, 
-    otherwise returns False.
-    """
-    if not os.path.exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
     try:
-        file_name = archive_path.split('/')[-1]
-        file_no_extension = file_name.split('.')[0]
-        put(archive_path, f'/tmp/{file_name}')
-        run(f'mkdir -p /data/web_static/releases/{file_no_extension}/')
-        run(f"tar -xzf /tmp/{file_name} -C /data/web_static/releases/{file_no_extension}/")
-        run(f'rm /tmp/{file_name}')
-        run(f'mv /data/web_static/releases/{file_no_extension}/web_static/* /data/web_static/releases/{file_no_extension}/')
-        run(f'rm -rf /data/web_static/releases/{file_no_extension}/web_static')
-        run(f'rm -rf /data/web_static/current')
-        run(f'ln -s /data/web_static/releases/{file_no_extension}/ /data/web_static/current')
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
-
-def deploy():
-    """calls do_pack and do_deploy"""
-    archive_path = do_pack()
-    if archive_path is None:
-        return False
-    return do_deploy(archive_path)
