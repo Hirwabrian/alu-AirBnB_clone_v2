b75c43e38acfea70af009126d3e70b6c97b01b22
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-10-05 21:56:33 +0200

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Fabric script that generates a tgz archive from the contents of the web_static
-folder of the AirBnB Clone repo
+folder of the AirBnB Clone repo.
 """
 
 from datetime import datetime
@@ -10,15 +10,35 @@ from os.path import isdir
 
 
 def do_pack():
-    """Generates a tgz archive."""
+    """
+    Generates a tgz archive of the web_static folder.
+
+    The archive is named using the current date and time in the format 
+    YYYYMMDDHHMMSS and stored in the 'versions' directory. If the 'versions' 
+    directory does not exist, it will be created.
+
+    Returns:
+        str: The path to the created tgz archive if successful, 
+             or None if an error occurs.
+    """
     try:
-        time = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
+        # Get the current date and time to create a unique archive name
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        
+        # Check if the 'versions' directory exists; create it if it doesn't
+        if not isdir("versions"):
             local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(time)
-        connection = Connection('localhost')  # Use a local connection
+
+        # Define the filename for the archive
+        file_name = "versions/web_static_{}.tgz".format(date)
+
+        # Create a tgz archive of the web_static folder
+        connection = Connection('localhost')  # Create a local connection
         connection.local("tar -cvzf {} web_static".format(file_name))
+
+        # Return the path of the created archive
         return file_name
     except Exception as e:
+        # Print the error message for debugging purposes
         print(f"Error: {e}")
         return None
