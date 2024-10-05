3325bf8d897c7210a4ed529637cee197b56fc934
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-10-05 13:50:21 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,16 +14,17 @@ def do_deploy(archive_path):
     if exists(archive_path) is False:
         return False
     try:
-        file_name = archive_path.split('/')[-1]
-        file_no_extension = file_name.split('.')[0]
-        put(archive_path, f'/tmp/{file_name}')
-        run(f'mkdir -p /data/web_static/releases/{file_no_extension}/')
-        run(f"tar -xzf /tmp/{file_name} -C /data/web_static/releases/{file_no_extension}/")
-        run(f'rm /tmp/{file_name}')
-        run(f'mv /data/web_static/releases/{file_no_extension}/web_static/* /data/web_static/releases/{file_no_extension}/')
-        run(f'rm -rf /data/web_static/releases/{file_no_extension}/web_static')
-        run(f'rm -rf /data/web_static/current')
-        run(f'ln -s /data/web_static/releases/{file_no_extension}/ /data/web_static/current')
+        file_name = archive_path.split("/")[-1]
+        file_no_extension = file_name.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, file_no_extension))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, file_no_extension))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_no_extension))
+        run('rm -rf {}{}/web_static'.format(path, file_no_extension))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, file_no_extension))
         return True
     except:
         return False
