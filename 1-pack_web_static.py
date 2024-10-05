d00fa93e15dbe69a3ee793ba079838dbbd72ab00
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-10-05 20:56:25 +0200

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,43 +1,22 @@
 #!/usr/bin/python3
 """
-Fabric script that generates a .tgz archive from the contents 
-of the web_static folder of the AirBnB Clone repository.
+Fabric script that generates a tgz archive from the contents of the web_static
+folder of the AirBnB Clone repo
 """
 
 from datetime import datetime
-from fabric import task
+from fabric.api import local
 from os.path import isdir
 
-@task
-def do_pack(c):
-    """
-    Generates a .tgz archive from the contents of the web_static folder.
 
-    Args:
-        c (Connection): A Fabric Connection object that provides 
-                        methods for running commands on a remote server.
-
-    Returns:
-        str: The path to the generated .tgz file if successful; 
-              None if there was an error during the process.
-    """
+def do_pack():
+    """generates a tgz archive"""
     try:
-        # Generate a timestamp to create a unique filename
         date = datetime.now().strftime("%Y%m%d%H%M%S")
-
-        # Check if the "versions" directory exists; if not, create it
-        if not isdir("versions"):
-            c.run("mkdir versions")
-        
-        # Define the name of the archive file
+        if isdir("versions") is False:
+            local("mkdir versions")
         file_name = "versions/web_static_{}.tgz".format(date)
-
-        # Create a .tgz archive of the web_static folder
-        c.run("tar -cvzf {} web_static".format(file_name))
-
-        # Return the path to the created archive
+        local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except Exception as e:
-        # Print the error message and return None if an exception occurs
-        print(f"Error: {e}")
+    except:
         return None
