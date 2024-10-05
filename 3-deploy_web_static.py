46f814e086d983e76049988db9e62b53cdbc413e
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-10-05 23:00:38 +0200

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """
 Fabric script based on the file 2-do_deploy_web_static.py that creates and
@@ -34,12 +33,14 @@ def do_deploy(archive_path):
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(path, file_no_extension))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(filename, path, file_no_extension))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(filename, path,
+                                               file_no_extension))
         run('rm /tmp/{}'.format(filename))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_no_extension))
         run('rm -rf {}{}/web_static'.format(path, file_no_extension))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, file_no_extension))
+        run('ln -s {}{}/ /data/web_static/current'.format(path,
+                                                          file_no_extension))
         return True
     except:
         return False
